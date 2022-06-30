755a50ac3f037433b8d502f2bf07bccbde8cbe94
Andrei <geleon21705@gmail.com>
2022-06-29 23:43:27 -0700

    added less 42, get elements from index.html(class,id,cssSelectors)

---
+++ js/less_42_get_element_from_DOM.js
@@ -0,0 +1,5 @@
+const box         = document.getElementById(`box`);
+const btns        = document.getElementsByTagName(`button`);
+const circles     = document.getElementsByClassName(`circle`);
+const hearts      = document.querySelectorAll(`.heart`);
+const oneHeart    = document.querySelector(`heart`);
