2f361bce075dab8b6d5f3717877203186e6293ac
Andrei <geleon21705@gmail.com>
2022-07-04 04:03:08 -0700

    getting element of documetn.getElement with css selector.

--- js/less_42_get_element_from_DOM.js
+++
@@ -1,5 +0,0 @@
-const box         = document.getElementById(`box`);
-const btns        = document.getElementsByTagName(`button`);
-const circles     = document.getElementsByClassName(`circle`);
-const hearts      = document.querySelectorAll(`.heart`);
-const oneHeart    = document.querySelector(`heart`);
